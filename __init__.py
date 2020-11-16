d449f9d4b7db2f738c54a88f21c698ec9e8719ce
ThoreBor <thore.tyborski@gmail.com>
2020-11-16 14:06:42 +0100

    minor changes

--- __init__.py
+++ __init__.py
@@ -2,16 +2,23 @@ from aqt import mw, gui_hooks
 from aqt.utils import tooltip
 
 def force_stop_reviews(card):
+	total_cards = to_learn()
 	config = mw.addonManager.getConfig(__name__)
-	currently = config["current_reviews"]
-	currently += 1
-	config = {"max_reviews": config["max_reviews"], "current_reviews": currently}
+	config = {"reviews_left": config["reviews_left"]}
 	mw.addonManager.writeConfig(__name__, config)
 
-	if currently >= config["max_reviews"]:
-		tooltip(f"You reviewed {currently} cards.")
+	if total_cards == config["reviews_left"]:
+		tooltip(f"You now have {total_cards} cards left to review.")
 		mw.moveToState("deckBrowser")
-		config = {"max_reviews": config["max_reviews"], "current_reviews": 0}
-		mw.addonManager.writeConfig(__name__, config)
+
+def to_learn():
+	new = 0
+	due = 0
+	decks = mw.col.sched.deck_due_tree().children
+	for deck in decks:
+		new += deck.new_count
+		due += deck.review_count + deck.learn_count
+
+	return due + new
 
 gui_hooks.reviewer_did_show_question.append(force_stop_reviews)
